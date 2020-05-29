b1385c77e2890ff44edf2a45c85a9ed9eba41fe7
phillipug <phillipmusiime@gmail.com>
2020-05-29 16:57:55 +0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+	root 'posts#index'
+  devise_for :members, controllers: {registrations: :registrations}
+  resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
