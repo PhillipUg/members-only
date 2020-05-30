4cfc45079858b5b65df4b37ee1cff4524ad39177
phillipug <phillipmusiime@gmail.com>
2020-05-30 09:55:07 +0300

    modified controllers to allow onlu new and create actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[show edit update destroy]
-  before_action :authenticate_member!, except: %i[index show]
+  # before_action :set_post, only: %i[show edit update destroy]
+  before_action :authenticate_member!, only: %i[new create]
 
   # GET /posts
   def index
@@ -8,16 +8,10 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  # GET /posts/1
-  def show; end
-
   # GET /posts/new
   def new
     @post = current_member.posts.build
   end
-
-  # GET /posts/1/edit
-  def edit; end
 
   # POST /posts
   def create
@@ -30,27 +24,12 @@ class PostsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /posts/1
-  def update
-    if @post.update(post_params)
-      redirect_to root_path, notice: 'Post was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  # DELETE /posts/1
-  def destroy
-    @post.destroy
-    redirect_to posts_url, notice: 'Post was successfully destroyed.'
-  end
-
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_post
-    @post = Post.find(params[:id])
-  end
+  # # Use callbacks to share common setup or constraints between actions.
+  # def set_post
+  #   @post = Post.find(params[:id])
+  # end
 
   # Only allow a trusted parameter "white list" through.
   def post_params
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,7 +5,7 @@ class RegistrationsController < Devise::RegistrationsController
     params.require(:member).permit(:username, :email, :password, :password_confirmation)
   end
 
-  def account_update_params
-    params.require(:member).permit(:username, :email, :password, :password_confirmation, :current_password)
-  end
+  # def account_update_params
+  #   params.require(:member).permit(:username, :email, :password, :password_confirmation, :current_password)
+  # end
 end
